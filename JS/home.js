3d815ea0b88f7a0e1fd0350c46e6330bc9ec0585
NaGit09 <nhutanhtran806@gmail.com>
2024-10-25 22:02:10 +0700

    push code

---
+++ JS/home.js
@@ -0,0 +1,12 @@
+const right_icon = document.getElementById("bi-arrow-right-circle-fill");
+console.log(right_icon);
+
+const listCard = document.getElementsByClassName("card") ;
+console.log(listCard);
+
+let index = 3;
+let i = 0;
+right_icon.addEventListener("click", () => {
+        
+    
+    })
